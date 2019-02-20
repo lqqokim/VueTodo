0ce8ca492c6befba62e5ea528ab182900ba9fff0
Tony Kim <kis420404@gmail.com>
2019-02-20 18:31:07 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!-- <h1>My Todo List</h1> -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
